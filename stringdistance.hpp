89c13a58218c867353a02e755e453057ca620b62
Morihiro Hayashida <morihiro@matsue-ct.jp>
2018-04-27 15:45:17 +0900

    first commit

---
+++ stringdistance.hpp
@@ -0,0 +1,252 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <random>
+#include <boost/math/distributions/normal.hpp>
+#include <boost/multi_array.hpp>
+
+struct StringDistance {
+  virtual int operator()(const std::vector<int>& x, const std::vector<int>& y) = 0;
+};
+
+struct ExtendedHammingDistance : public StringDistance {
+    int operator()(const std::vector<int>& x, const std::vector<int>& y) {
+      int xsize = x.size();
+      int ysize = y.size();
+      int minsize = ysize;
+      int cost = xsize - ysize;
+      if (cost < 0) {
+        minsize = xsize;
+        cost = -cost;
+      }
+      for (int i = 0; i < minsize; i++) {
+        if (x[i] != y[i]) {
+          cost++;
+        }
+      }
+      return cost;
+    }
+};
+
+struct LongestCommonSubsequenceDistance : public StringDistance {
+    int operator()(const std::vector<int>& x, const std::vector<int>& y) {
+      int xsize = x.size();
+      int ysize = y.size();
+      boost::multi_array<int, 2> d(boost::extents[xsize + 1][ysize + 1]);
+      for (int i = 0; i <= xsize; i++) {
+        d[i][0] = 0;
+      }
+      for (int j = 1; j <= ysize; j++) {
+        d[0][j] = 0;
+      }
+      for (int i = 1; i <= xsize; i++) {
+        for (int j = 1; j <= ysize; j++) {
+          if (x[i - 1] == y[j - 1]) {
+            d[i][j] = d[i - 1][j - 1] + 1;
+          } else {
+            d[i][j] = std::max(d[i - 1][j], d[i][j - 1]);
+          }
+        }
+      }
+      return d[xsize][ysize];
+    }
+};
+
+struct LevenshteinDistance : public StringDistance {
+    int operator()(const std::vector<int>& x, const std::vector<int>& y) {
+      const int InsertCost = 1;
+      const int DeleteCost = 1;
+      const int SubstCost = 1;
+      int xsize = x.size();
+      int ysize = y.size();
+      boost::multi_array<int, 2> d(boost::extents[xsize][ysize]);
+
+      for (int i = 0; i <= xsize; i++) {
+        d[i][0] = i;
+      }
+      for (int j = 1; j <= ysize; j++) {
+        d[0][j] = j;
+      }
+      for (int i = 1; i <= xsize; i++) {
+        for (int j = 1; j <= ysize; j++) {
+          int cost = (x[i - 1] == y[j - 1]) ? 0 : SubstCost;
+          d[i][j] = std::min(std::min(
+              d[i - 1][j] + InsertCost,
+              d[i][j - 1] + DeleteCost),
+              d[i - 1][j - 1] + cost);
+        }
+      }
+      return d[xsize][ysize];
+    }
+};
+
+struct DamerauLevenshteinDistance : public StringDistance {
+    int operator()(const std::vector<int>& x, const std::vector<int>& y) {
+      return 0;
+    }
+};
+
+struct JaroWinklerDistance : public StringDistance {
+    int operator()(const std::vector<int>& x, const std::vector<int>& y) {
+      return 0;
+    }
+};
+
+static void print(const std::vector<int>& s) {
+  for (auto c : s) {
+    std::cout << c;
+  }
+  std::cout << "\n";
+}
+
+template <typename DistancePred>
+void count(int& tildeu, int& tildev, // estimated values
+    int& hatu, int& hatv, // strong eastimated values
+    const int k,  // the size of alphabet
+    const std::vector<int>& center, // center string
+    const int radius, // radius
+    DistancePred distfunc, // distance function
+    const double alpha, // quantile of gaussian distribution
+    const int lowerbound, // the lower bound of # randomly generated strings
+    const int iterations, // convergence test
+    const int nmax = INT_MAX  // max # randomly generated strings
+    ) {
+  std::random_device rd;
+  std::mt19937 mt(rd());
+  int lstart = center.size() - radius;
+  int lend = center.size() + radius;
+
+  double kmc = std::pow(k, lend) - ((lstart >= 1) ? std::pow(k, lstart - 1) : 0.0); // k^(|s|+r) - c
+
+  if (lstart < 0) {
+    lstart = 0;
+  }
+  std::uniform_int_distribution<int> lgen(lstart, lend);
+  std::uniform_int_distribution<int> agen(0, k-1);
+
+  boost::math::normal gauss(0.0, 1.0);
+  double z = quantile(gauss, 1.0 - alpha * 0.5);
+  double z2 = z * z; // z^2
+  double z24 = z2 * 4; // 4z^2
+  double z4 = z2 * z2; // z^4
+ 
+  //print(center);
+
+  double xn = 0;
+  double yn = 0;
+  bool uflag = true;
+  bool vflag = true;
+  int bu = iterations;
+  int bv = iterations;
+  int preun = INT_MAX;
+  int prevn = INT_MAX;
+  std::vector<int> sn(center.size() + radius);
+  for (int n = 1; (bu > 0 or bv > 0 or uflag or vflag) and n < nmax; n++) {
+    int l = lgen(mt);
+    sn.resize(l);
+    for (int i = 0; i < l; i++) {
+      sn[i] = agen(mt);
+    }
+    //print(sn);
+    int d = distfunc(center, sn);
+    //std::cout << "d: " << d << "\n";
+    if (d <= radius) {
+      xn++;
+      if (d == radius) {
+        yn++;
+      }
+    }
+    double tmp = kmc / n;
+    int un = (int)(0.5 + tmp * xn);
+    int vn = (int)(0.5 + tmp * yn);
+    if (uflag) {
+      if ((n > lowerbound) and (n > kmc * sqrt(z24 * xn * (1.0 - xn/n) + z4) - z2)) {
+        tildeu = un;
+        uflag = false;
+      }
+    }
+    if (vflag) {
+      if ((n > lowerbound) and (n > kmc * sqrt(z24 * yn * (1.0 - yn/n) + z4) - z2)) {
+        tildev = vn;
+        vflag = false;
+      }
+    }
+    if (bu > 0) {
+      if (un == preun) {
+        bu--;
+        if (bu == 0) {
+          hatu = un;
+        }
+      } else {
+        bu = iterations;
+      }
+    }
+    if (bv > 0) {
+      if (vn == prevn) {
+        bv--;
+        if (bv == 0) {
+          hatv = vn;
+        }
+      } else {
+        bv = iterations;
+      }
+    }
+    preun = un;
+    prevn = vn;
+  }
+  if (bu > 0 or bv > 0 or uflag or vflag) {
+    std::cerr << "# iterations reached max\n";
+  }
+}
+
+template <typename DistancePred>
+void searchall(int& u, int& v, // estimated values
+    const int k,  // the size of alphabet
+    const std::vector<int>& center, // center string
+    const int radius, // radius
+    DistancePred distfunc, // distance function
+    //StringDistance& distfunc,
+    const double alpha, // quantile of gaussian distribution
+    const int nmax = INT_MAX  // max # randomly generated strings
+    ) {
+  std::vector<int> sn(center.size() + radius);
+  int xn = 0;
+  int yn = 0;
+  int lstart = center.size() - radius;
+  int lend = center.size() + radius;
+  if (lstart < 0) {
+    lstart = 0;
+  }
+  for (int l = lstart; l <= lend; l++) {
+    sn.resize(l);
+    for (int i = 0; i < l; i++) {
+      sn[i] = 0;
+    }
+    bool cf = true;
+    while (cf) {
+      //print(sn);
+      int d = distfunc(center, sn);
+      //std::cout << "d: " << d << "\n";
+      if (d <= radius) {
+        xn++;
+        if (d == radius) {
+          yn++;
+        }
+      }
+      for (int i = 0; cf and i < l; i++) {
+        sn[i]++;
+        if (sn[i] == k) {
+          cf = true;
+          sn[i] = 0;
+        } else {
+          cf = false;
+        }
+      }
+      cf = not cf;
+    }
+  }
+
+  u = xn;
+  v = yn;
+}
+
---
+++ stringsphere.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <boost/program_options.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/lexical_cast.hpp>
+#include <string>
+#include <vector>
+#include "stringdistance.hpp"
+#include <typeinfo>
+
+std::vector<int> parsestring(const std::string& s)
+{
+  std::vector<std::string> t;
+  boost::algorithm::split(t, s, boost::is_any_of(","));
+  std::vector<int> res(t.size());
+  for (size_t i = 0; i < t.size(); i++) {
+    res[i] = boost::lexical_cast<int>(t[i]);
+  }
+  return res;
+}
+
+int main(int argc, char **argv)
+{
+  using namespace std;
+  namespace po = boost::program_options;
+  po::options_description opt("Allowed options");
+
+  opt.add_options()
+    ("distance,d", po::value<int>()->default_value(0), ": 0: Extended Hamming 1: Longest common subsequence 2: Levenshtein 3: Damerau-Levenshtein 4: Jaro-Winkler")
+    ("alpha,a", po::value<double>()->default_value(0.1), ": 100xalpha/2 percent point of gaussian")
+    ("k,k", po::value<int>()->default_value(2), ": the size of alphabet A")
+    ("center,c", po::value<std::string>()->default_value("0,0,0"), ": center string in integers from 0 to k-1 with comma delimiter")
+    ("radius,r", po::value<int>()->default_value(2), ": radius")
+    ("lowerbound,l", po::value<int>()->default_value(10000), ": the lower bound of # randomly generated strings")
+    ("iterations,i", po::value<int>()->default_value(100), ": # of iterations that estimated values are equal (convergence test).")
+    ("searchall,s", ": search all instead of the stochastic method")
+    ("help,h", ": show this help message");
+
+  po::variables_map argmap;
+  po::store(po::parse_command_line(argc, argv, opt), argmap);
+  po::notify(argmap);
+
+  if (argmap.count("help")) {
+    cerr << "Usage: " << argv[0] << " [option]" << endl << opt << endl;
+
+    return EXIT_FAILURE;
+  }
+
+  int distancetype = argmap["distance"].as<int>();
+  double alpha = argmap["alpha"].as<double>();
+  int k = argmap["k"].as<int>();
+  auto center = parsestring(argmap["center"].as<std::string>());
+  int radius = argmap["radius"].as<int>();
+  int lowerbound = argmap["lowerbound"].as<int>();
+  int iterations = argmap["iterations"].as<int>();
+
+  if (argmap.count("searchall")) {
+    int u, v;
+    switch (distancetype) {
+      case 0:
+        searchall(u, v, k, center, radius, ExtendedHammingDistance(), alpha);
+        break;
+      case 1:
+        searchall(u, v, k, center, radius, LongestCommonSubsequenceDistance(), alpha);
+        break;
+      case 2:
+        searchall(u, v, k, center, radius, LevenshteinDistance(), alpha);
+        break;
+      case 3:
+        searchall(u, v, k, center, radius, DamerauLevenshteinDistance(), alpha);
+        break;
+      case 4:
+        searchall(u, v, k, center, radius, JaroWinklerDistance(), alpha);
+        break;
+    };
+    std::cout << "u = " << u << "\tv = " << v << "\n";
+  } else {
+    int tildeu, tildev, hatu, hatv;
+    switch (distancetype) {
+      case 0:
+        count(tildeu, tildev, hatu, hatv, k, center, radius, ExtendedHammingDistance(), alpha, lowerbound, iterations);
+        break;
+      case 1:
+        count(tildeu, tildev, hatu, hatv, k, center, radius, LongestCommonSubsequenceDistance(), alpha, lowerbound, iterations);
+        break;
+      case 2:
+        count(tildeu, tildev, hatu, hatv, k, center, radius, LevenshteinDistance(), alpha, lowerbound, iterations);
+        break;
+      case 3:
+        count(tildeu, tildev, hatu, hatv, k, center, radius, DamerauLevenshteinDistance(), alpha, lowerbound, iterations);
+        break;
+      case 4:
+        count(tildeu, tildev, hatu, hatv, k, center, radius, JaroWinklerDistance(), alpha, lowerbound, iterations);
+        break;
+    };
+    std::cout << "~u = " << tildeu << "\t~v = " << tildev << "\t ^u = " << hatu << "\t ^v = " << hatv << "\n";
+  }
+
+  return EXIT_SUCCESS;
+}
+
