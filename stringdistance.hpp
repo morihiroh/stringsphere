924cecf07b74914ff4463086c00aa889ea6e0708
Morihiro Hayashida <morihiro@matsue-ct.jp>
2018-06-18 11:08:48 +0900

    added measure and within

--- stringdistance.hpp
+++ stringdistance.hpp
@@ -7,157 +7,166 @@
 
 #define DISABLE_JAROWINKLER
 
-struct StringDistance {
-  virtual int operator()(const std::vector<int>& x, const std::vector<int>& y) = 0;
+struct CompareDistance {};
+struct CompareSimilarity {};
+struct ExtendedHammingDistance {
+  typedef CompareDistance CompareType;
+};
+struct LongestCommonSubsequenceDistance {
+  typedef CompareSimilarity CompareType;
+};
+struct LevenshteinDistance {
+  typedef CompareDistance CompareType;
+};
+struct DamerauLevenshteinDistance {
+  typedef CompareDistance CompareType;
 };
 
-struct ExtendedHammingDistance : public StringDistance {
-    int operator()(const std::vector<int>& x, const std::vector<int>& y) {
-      int xsize = x.size();
-      int ysize = y.size();
-      int minsize = ysize;
-      int cost = xsize - ysize;
-      if (cost < 0) {
-        minsize = xsize;
-        cost = -cost;
-      }
-      for (int i = 0; i < minsize; i++) {
-        if (x[i] != y[i]) {
-          cost++;
-        }
-      }
-      return cost;
+int measure(const std::vector<int>& x, const std::vector<int>& y, ExtendedHammingDistance) {
+  int xsize = x.size();
+  int ysize = y.size();
+  int minsize = ysize;
+  int cost = xsize - ysize;
+  if (cost < 0) {
+    minsize = xsize;
+    cost = -cost;
+  }
+  for (int i = 0; i < minsize; i++) {
+    if (x[i] != y[i]) {
+      cost++;
     }
-};
+  }
+  return cost;
+}
 
-struct LongestCommonSubsequenceDistance : public StringDistance {
-    int operator()(const std::vector<int>& x, const std::vector<int>& y) {
-      int xsize = x.size();
-      int ysize = y.size();
-      boost::multi_array<int, 2> d(boost::extents[xsize + 1][ysize + 1]);
-      for (int i = 0; i <= xsize; i++) {
-        d[i][0] = 0;
+int measure(const std::vector<int>& x, const std::vector<int>& y, LongestCommonSubsequenceDistance) {
+  int xsize = x.size();
+  int ysize = y.size();
+  boost::multi_array<int, 2> d(boost::extents[xsize + 1][ysize + 1]);
+  for (int i = 0; i <= xsize; i++) {
+    d[i][0] = 0;
+  }
+  for (int j = 1; j <= ysize; j++) {
+    d[0][j] = 0;
+  }
+  for (int i = 1; i <= xsize; i++) {
+    for (int j = 1; j <= ysize; j++) {
+      if (x[i - 1] == y[j - 1]) {
+        d[i][j] = d[i - 1][j - 1] + 1;
+      } else {
+        d[i][j] = std::max(d[i - 1][j], d[i][j - 1]);
       }
-      for (int j = 1; j <= ysize; j++) {
-        d[0][j] = 0;
-      }
-      for (int i = 1; i <= xsize; i++) {
-        for (int j = 1; j <= ysize; j++) {
-          if (x[i - 1] == y[j - 1]) {
-            d[i][j] = d[i - 1][j - 1] + 1;
-          } else {
-            d[i][j] = std::max(d[i - 1][j], d[i][j - 1]);
-          }
-        }
-      }
-      return d[xsize][ysize];
     }
-};
+  }
+  return d[xsize][ysize];
+}
 
-struct LevenshteinDistance : public StringDistance {
-    int operator()(const std::vector<int>& x, const std::vector<int>& y) {
-      const int InsertCost = 1;
-      const int DeleteCost = 1;
-      const int SubstCost = 1;
-      int xsize = x.size();
-      int ysize = y.size();
-      boost::multi_array<int, 2> d(boost::extents[xsize + 1][ysize + 1]);
+int measure(const std::vector<int>& x, const std::vector<int>& y, LevenshteinDistance) {
+  const int InsertCost = 1;
+  const int DeleteCost = 1;
+  const int SubstCost = 1;
+  int xsize = x.size();
+  int ysize = y.size();
+  boost::multi_array<int, 2> d(boost::extents[xsize + 1][ysize + 1]);
 
-      for (int i = 0; i <= xsize; i++) {
-        d[i][0] = i;
-      }
-      for (int j = 1; j <= ysize; j++) {
-        d[0][j] = j;
-      }
-      for (int i = 1; i <= xsize; i++) {
-        for (int j = 1; j <= ysize; j++) {
-          int cost = (x[i - 1] == y[j - 1]) ? 0 : SubstCost;
-          d[i][j] = std::min(std::min(
-              d[i - 1][j] + InsertCost,
-              d[i][j - 1] + DeleteCost),
-              d[i - 1][j - 1] + cost);
-        }
-      }
-      return d[xsize][ysize];
+  for (int i = 0; i <= xsize; i++) {
+    d[i][0] = i;
+  }
+  for (int j = 1; j <= ysize; j++) {
+    d[0][j] = j;
+  }
+  for (int i = 1; i <= xsize; i++) {
+    for (int j = 1; j <= ysize; j++) {
+      int cost = (x[i - 1] == y[j - 1]) ? 0 : SubstCost;
+      d[i][j] = std::min(std::min(
+            d[i - 1][j] + InsertCost,
+            d[i][j - 1] + DeleteCost),
+          d[i - 1][j - 1] + cost);
     }
-};
+  }
+  return d[xsize][ysize];
+}
 
-struct DamerauLevenshteinDistance : public StringDistance {
-    int operator()(const std::vector<int>& x, const std::vector<int>& y) {
-      const int InsertCost = 1;
-      const int DeleteCost = 1;
-      const int SubstCost = 1;
-      int xsize = x.size();
-      int ysize = y.size();
-      boost::multi_array<int, 2> d(boost::extents[xsize + 1][ysize + 1]);
+int measure(const std::vector<int>& x, const std::vector<int>& y, DamerauLevenshteinDistance) {
+  const int InsertCost = 1;
+  const int DeleteCost = 1;
+  const int SubstCost = 1;
+  int xsize = x.size();
+  int ysize = y.size();
+  boost::multi_array<int, 2> d(boost::extents[xsize + 1][ysize + 1]);
 
-      for (int i = 0; i <= xsize; i++) {
-        d[i][0] = i;
+  for (int i = 0; i <= xsize; i++) {
+    d[i][0] = i;
+  }
+  for (int j = 1; j <= ysize; j++) {
+    d[0][j] = j;
+  }
+  for (int i = 1; i <= xsize; i++) {
+    for (int j = 1; j <= ysize; j++) {
+      int cost = (x[i - 1] == y[j - 1]) ? 0 : SubstCost;
+      d[i][j] = std::min(std::min(
+            d[i - 1][j] + InsertCost,
+            d[i][j - 1] + DeleteCost),
+          d[i - 1][j - 1] + cost);
+      if (i > 1 and j > 1 and x[i - 1] == y[j - 2] and x[i - 2] == y[j - 1]) {
+        d[i][j] = std::min(d[i][j], d[i - 2][j - 2] + cost);
       }
-      for (int j = 1; j <= ysize; j++) {
-        d[0][j] = j;
-      }
-      for (int i = 1; i <= xsize; i++) {
-        for (int j = 1; j <= ysize; j++) {
-          int cost = (x[i - 1] == y[j - 1]) ? 0 : SubstCost;
-          d[i][j] = std::min(std::min(
-              d[i - 1][j] + InsertCost,
-              d[i][j - 1] + DeleteCost),
-              d[i - 1][j - 1] + cost);
-          if (i > 1 and j > 1 and x[i - 1] == y[j - 2] and x[i - 2] == y[j - 1]) {
-            d[i][j] = std::min(d[i][j], d[i - 2][j - 2] + cost);
-          }
-        }
-      }
-      return d[xsize][ysize];
     }
-};
+  }
+  return d[xsize][ysize];
+}
+
+bool within(const int d, const int radius, CompareDistance) {
+  return d <= radius;
+}
+
+bool within(const int d, const int radius, CompareSimilarity) {
+  return d >= radius;
+}
 
 #ifndef DISABLE_JAROWINKLER
-struct JaroWinklerDistance {
-  double operator()(const std::vector<int>& x, const std::vector<int>& y) {
-    int xsize = x.size();
-    int ysize = y.size();
-    if (xsize == 0) {
-      if (ysize == 0)
-        return 1.0;
-      else
-        return 0.0;
-    }
-    int match_dist = std::max(xsize, ysize)/2 - 1;
-    std::vector<bool> x_matches(xsize, false), y_matches(ysize, false);
-    double m = 0.0;
-    double t = 0.0;
-    for (int i = 0; i < xsize; i++) {
-      const int start = std::max(0, i - match_dist);
-      const int end = std::min(i + match_dist + 1, ysize);
-      for (int j = start; j < end; j++) {
-        if (y_matches[j]) continue;
-        if (x[i] != y[j]) continue;
-        x_matches[i] = true;
-        y_matches[j] = true;
-        m++;
-        break;
-      }
-    }
-    if (m == 0) 
+double operator()(const std::vector<int>& x, const std::vector<int>& y, JaroWinklerDistance) {
+  int xsize = x.size();
+  int ysize = y.size();
+  if (xsize == 0) {
+    if (ysize == 0)
+      return 1.0;
+    else
       return 0.0;
+  }
+  int match_dist = std::max(xsize, ysize)/2 - 1;
+  std::vector<bool> x_matches(xsize, false), y_matches(ysize, false);
+  double m = 0.0;
+  double t = 0.0;
+  for (int i = 0; i < xsize; i++) {
+    const int start = std::max(0, i - match_dist);
+    const int end = std::min(i + match_dist + 1, ysize);
+    for (int j = start; j < end; j++) {
+      if (y_matches[j]) continue;
+      if (x[i] != y[j]) continue;
+      x_matches[i] = true;
+      y_matches[j] = true;
+      m++;
+      break;
+    }
+  }
+  if (m == 0) 
+    return 0.0;
 
-    int j = 0;
-    for (int i = 0; i < xsize; i++) {
-      if (not x_matches[i]) continue;
-      while (not y_matches[j]) {
-        j++;
-      }
-      if (x[i] != y[j]) {
-        t++;
-      }
+  int j = 0;
+  for (int i = 0; i < xsize; i++) {
+    if (not x_matches[i]) continue;
+    while (not y_matches[j]) {
       j++;
     }
-    t /= 2.0;
-    return (m/xsize + m/ysize + (m - t)/m)/3.0;
+    if (x[i] != y[j]) {
+      t++;
+    }
+    j++;
   }
-};
+  t /= 2.0;
+  return (m/xsize + m/ysize + (m - t)/m)/3.0;
+}
 #endif
 
 #if 0
@@ -169,13 +178,13 @@ static void print(const std::vector<int>& s) {
 }
 #endif
 
-template <typename DistancePred>
+template <typename DistanceTag>
 void count(int& tildeu, int& tildev, // estimated values
     int& hatu, int& hatv, // strong eastimated values
     const int k,  // the size of alphabet
     const std::vector<int>& center, // center string
     const int radius, // radius
-    DistancePred distfunc, // distance function
+    DistanceTag, // distance function
     const double alpha, // quantile of gaussian distribution
     const int lowerbound, // the lower bound of # randomly generated strings
     const int iterations, // convergence test
@@ -199,7 +208,7 @@ void count(int& tildeu, int& tildev, // estimated values
   double z2 = z * z; // z^2
   double z24 = z2 * 4; // 4z^2
   double z4 = z2 * z2; // z^4
- 
+
   //print(center);
 
   double xn = 0;
@@ -218,9 +227,9 @@ void count(int& tildeu, int& tildev, // estimated values
       sn[i] = agen(mt);
     }
     //print(sn);
-    auto d = distfunc(center, sn);
+    auto d = measure(center, sn, DistanceTag());
     //std::cout << "d: " << d << "\n";
-    if (d <= radius) {
+    if (within(d, radius, typename DistanceTag::CompareType())) {
       xn++;
       if (d == radius) {
         yn++;
@@ -269,13 +278,12 @@ void count(int& tildeu, int& tildev, // estimated values
   }
 }
 
-template <typename DistancePred>
+template <typename DistanceTag>
 void searchall(int& u, int& v, // estimated values
     const int k,  // the size of alphabet
     const std::vector<int>& center, // center string
     const int radius, // radius
-    DistancePred distfunc, // distance function
-    //StringDistance& distfunc,
+    DistanceTag, // distance function
     const double alpha, // quantile of gaussian distribution
     const int nmax = INT_MAX  // max # randomly generated strings
     ) {
@@ -295,9 +303,9 @@ void searchall(int& u, int& v, // estimated values
     bool cf = true;
     while (cf) {
       //print(sn);
-      int d = distfunc(center, sn);
+      int d = measure(center, sn, DistanceTag());
       //std::cout << "d: " << d << "\n";
-      if (d <= radius) {
+      if (within(d, radius, typename DistanceTag::CompareType())) {
         xn++;
         if (d == radius) {
           yn++;
